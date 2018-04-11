c4c8a7f757b73733172e5001a846f9a74caf650d
eliotcarlsen <eliot.carlsen@ispot.tv>
2018-04-11 15:43:57 -0700

    add story component

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Story from "./Story";
 
 function Stories() {
   return (
     <h3>Stories</h3>
+      <Story />
   );
 }
 
---
+++ src/components/Story.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Story() {
+    return (
+        <h3>Story</h3>
+        <div>
+        <ul>
+        <li>link</li>
+        <li>link</li>
+        <li>link</li>
+        <li>link</li>
+        </ul>
+        </div>
+    );
+}
+
+export default Story;
